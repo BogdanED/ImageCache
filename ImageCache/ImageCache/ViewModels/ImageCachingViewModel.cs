4002048ec1f10e8c951c3fdfcf834b1bd3a2250c
Bogdan Dan <bdan@shift7digital.com>
2022-06-23 12:22:51 +0300

    Add project files.

---
+++ ImageCache/ImageCache/ViewModels/ImageCachingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ImageCache.ViewModels
+{
+    public class ImageCachingViewModel : BaseViewModel
+    {
+        public UriImageSource Image { get; set; } =
+            new UriImageSource
+            {
+                Uri = new Uri("https://image.webcamromania.ro/camera/045/snap/cam_share.1655814608.jpg"),
+                CacheValidity = new TimeSpan(0, 0, 1, 0)
+            };
+    }
+}
---
+++ ImageCache/ImageCache/Views/ImageCachingPage.xaml.cs
@@ -0,0 +1,14 @@
+using ImageCache.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ImageCache.Views
+{
+    public partial class ImageCachingPage : ContentPage
+    {
+        public ImageCachingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
